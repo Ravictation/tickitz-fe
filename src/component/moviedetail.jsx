4191e9af704feec682ce77d584f693b1e5b23a9c
sasmeka <verdysas@gmail.com>
2023-08-27 22:53:52 +0700

    fix detail movie

--- src/component/moviedetail.jsx
+++ src/component/moviedetail.jsx
@@ -1,81 +1,88 @@
 import React from "react";
+import moment from "moment/moment";
 
-function MovieDetail ({bgFoto, genre, foto, title, directed,hour, time, release, casts, synopsis}){
-    return(
-    <div>
-        <div className="bg-cover bg-center">
-            <img src={bgFoto} alt="" className="object-cover w-full md:max-h-[300px] xs:h-[400px] xs:object-cover"/>
-        </div>
-
-        <div className="wrapper-detail md:px-10 lg:px-0 md:grid md:grid-cols-6 xs:flex xs:flex-col">
-            <div className="banner-detail md:grid md:col-start-1 md:col-span-2">
-                <div className="rounded-md mx-auto flex items-center md:max-h-[50px] xs:max-h-[10px] xs:w-full md:w-fit">
-                    <img src={foto} alt="" className="mx-auto md:h-[280px] md:w-fit xs:w-11/12 xs:object-cover"/>
-                </div>
+function MovieDetail({ bgFoto, genre, foto, title, directed, hour, time, release, casts, synopsis }) {
+    let s = ""
+    function capitalTitle(text) {
+        return (text.replace(/\w\S*/g, function (word) {
+            const newWord = word.slice(0, 1).toUpperCase() + word.substr(1);
+            return newWord
+        }))
+    }
+    return (
+        <div>
+            <div className="bg-cover bg-center">
+                <img src={bgFoto} alt="" className="object-cover w-full md:max-h-[300px] xs:h-[400px] xs:object-cover" />
             </div>
 
-            <div className="detail-info md:grid md:col-start-3 md:col-span-6 xs:mt-60 sm:mt-72 md:mt-0 xs:p-6 md:p-0">
-                <div className="wrapper-detail-info flex flex-col">
-                    <div className="wrapper-title-genre xs:text-center">
-                        <h3 className="text-2xl flex font-semibold mt-2 xs:text-center xs:justify-center md:justify-normal">{title}</h3>
-                        
-                        <div className="wrapper-genre flex text-font flex-row text-xs gap-x-3 mt-3 xs:justify-center md:justify-normal xs:mb-3 md:mb-0">
-                            {genre ? 
-                            genre.map((v)=>{
-                                return(
-                                    <p className="bg-placeholder rounded-md p-1 px-3 flex justify-center items-center">{v.name_genre}</p>
-                                )
-                            }): ""}
+            <div className="wrapper-detail md:px-10 lg:px-0 md:grid md:grid-cols-6 xs:flex xs:flex-col">
+                <div className="banner-detail md:grid md:col-start-1 md:col-span-2">
+                    <div className="rounded-md mx-auto flex justify-center items-center md:max-h-[70px] xs:max-h-[10px] xs:w-full md:w-fit">
+                        <div className="w-56 h-96">
+                            <img src={foto} alt="" className="h-full object-cover rounded-xl" />
                         </div>
                     </div>
+                </div>
 
-                    <div className="info-movie mt-1 grid grid-rows-2 grid-cols-2">
-                        <div className="box-info lg:py-1">
-                            <p className="info-in-top text-xs text-font mb-1">Release Date</p>
-                            <p className="info-in-bottom text-sm ">{release}</p>
+                <div className="detail-info md:grid md:col-start-3 md:col-span-6 xs:mt-60 sm:mt-25 md:mt-0 xs:p-6 md:p-0">
+                    <div className="wrapper-detail-info flex flex-col">
+                        <div className="wrapper-title-genre xs:text-center">
+                            <h3 className="text-3xl flex font-semibold mt-2 xs:text-center xs:justify-center md:justify-normal">{capitalTitle(title)}</h3>
+
+                            <div className="wrapper-genre flex text-font flex-row text-xs gap-x-3 mt-3 xs:justify-center md:justify-normal xs:mb-3 md:mb-0">
+                                {genre ?
+                                    genre.map((v) => {
+                                        return (
+                                            <p className="bg-[#A0A3BD] opacity-50 text-black rounded-md p-1 px-3 flex justify-center items-center">{capitalTitle(v.name_genre)}</p>
+                                        )
+                                    }) : ""}
+                            </div>
                         </div>
 
-                        <div className="col-8 box-info col lg:py-1">
-                            <p className="info-in-top text-xs text-font mb-1">Duration</p>
-                            <p className="info-in-bottom text-sm flex flex-row ">
-                            {hour} Hours {time} Minutes</p>
-                        </div>
-
-                        <div className="col-4 box-info lg:py-1">
-                            <p className="info-in-top text-xs text-font mb-1">Directed By</p>
-                            <p className="info-in-bottom text-sm ">{
-                                directed ?
-                                    directed.map((v)=>  v.name_director):""
-                            }
-                            </p>
-
-
-                        </div>
-
-                        <div className="col-8 box-info lg:py-1">
-                            <p className="info-in-top text-xs text-font mb-1">Casts</p>
-                            <div className="flex flex-row">
-                            {casts ?
-                                casts.map((v, i)=>{
-                                    return(
-                                        <p className="info-in-top text-xs text-font mb-1">{v.name_cast + (i+1 == casts.length ? '' : ',  '  )}</p>
-                                    )
-                                }):""
-                            }
+                        <div className="info-movie mt-5 grid grid-rows-2 grid-cols-2">
+                            <div className="box-info lg:pb-5">
+                                <p className="info-in-top text-md text-font mb-1">Release Date</p>
+                                <p className="info-in-bottom text-md md:text-xl">{moment.utc(release).utc().format('MMMM D, YYYY')}</p>
+                            </div>
+                            <div className="col-8 box-info col lg:pb-5">
+                                <p className="info-in-top text-md text-font mb-1">Duration</p>
+                                <p className="info-in-bottom text-md md:text-xl">
+                                    {hour} Hours {time} Minutes</p>
+                            </div>
+                            <div className="col-4 box-info lg:pb-5">
+                                <p className="info-in-top text-md text-font mb-1">Directed By</p>
+                                <p className="info-in-bottom text-md md:text-xl">{
+                                    directed ?
+                                        directed.map((v) => capitalTitle(v.name_director)) : ""
+                                }
+                                </p>
+                            </div>
+                            <div className="col-8 box-info lg:pb-5">
+                                <p className="info-in-top text-md text-font mb-1">Casts</p>
+                                <div className="flex flex-row">
+                                    {
+                                        casts ?
+                                            casts.map((v, i) => {
+                                                s = s + (i + 1 == casts.length ? v.name_cast : v.name_cast + ", ")
+                                                if (i + 1 == casts.length) {
+                                                    return (<p className="info-in-bottom text-md md:text-xl">{capitalTitle(s)}</p>)
+                                                }
+                                            }) : ""
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div className="wrappper-synopsis flex flex-col mb-10 xs:p-8 md:p-0 xs:-mt-8 md:mt-0 md:px-16 lg:px-20 lg:mt-5">
-        <h3>Synopsis</h3>
-        <p className="text-font text-sm text-justify">
-            {synopsis}
-        </p>
-    </div>
-    </div>
+            <div className="wrappper-synopsis flex flex-col mb-20 xs:p-8 md:p-0 xs:-mt-8 md:mt-5 md:px-16 lg:px-20 lg:mt-5">
+                <h3 className="info-in-top text-xl mb-1 font-semibold">Synopsis</h3>
+                <p className="info-in-bottom text-font text-md md:text-md">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
     )
 }
 
--- src/page/detail/index.jsx
+++ src/page/detail/index.jsx
@@ -1,234 +1,230 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from '../../component/navbar'
 import Footer from '../../component/footer'
 import { Link, useNavigate, useParams } from "react-router-dom";
 import useApi from '../../helpers/useApi'
 import moment from "moment/moment"
 import MovieDetail from '../../component/moviedetail'
-// import {useSelector, useDispatch} from 'react-redux';
-// import {addData} from'../../store/reducer/user'
+import Pagination from "../../component/pagination"
 
-function Detail (){
-    const params= useParams()
+function Detail() {
+    const params = useParams()
     const paramsId = params.id
     const [detail, setDetail] = useState([])
-    const [pickTime, setPickTime]=useState([])
-    const [pickDate, setPickDate]=useState([])
-    const [pickLoc, setPickLock]=useState([])
+    const [pickTime, setPickTime] = useState([])
+    const [pickDate, setPickDate] = useState([])
+    const [pickLoc, setPickLock] = useState([])
     const [sch, setSch] = useState([])
+    const [meta_sch, setmeta_Sch] = useState([])
+
+    const [pageactive, setpageactive] = useState(1)
     const [pickSch, setPickSch] = useState('')
-    // const dispatch = useDispatch()
-    // const {isAuth} = useSelector((s)=> s.users)
+
+    const [date, setdate] = useState("")
+    const [time, settime] = useState("")
+    const [loc, setloc] = useState("")
     const api = useApi()
     const navigate = useNavigate()
     const limitSch = 4
 
-
-    const getDetail = async () =>{
+    const getDetail = async () => {
         api({
-            method : 'GET',
-            url : `/movies/${paramsId}`,
-            data : detail
-            })             
-            .then(({data})=>{
+            method: 'GET',
+            url: `/movies/${paramsId}`,
+            data: detail
+        })
+            .then(({ data }) => {
                 setDetail(data.data)
                 setPickLock(data.data[0].locations)
                 setPickTime(data.data[0].times)
                 setPickDate(data.data[0].set_dates)
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err)
             })
-        }
+    }
 
-    const getSch = async()=>{
+    function capitalTitle(text) {
+        return (text.replace(/\w\S*/g, function (word) {
+            const newWord = word.slice(0, 1).toUpperCase() + word.substr(1);
+            return newWord
+        }))
+    }
+
+    const getSch = async () => {
         api({
             method: 'GET',
-            url: `times_schedules/?movie=${paramsId}&limit=${limitSch}`,
-            data: sch
+            url: `/times_schedules/?movie=${paramsId}&limit=${limitSch}&page=${pageactive}&location=${loc}&time=${time}&date=${date}`
         })
-        .then(({data})=>{
-            setSch(data.data)
-        })            
-        .catch((err)=>{
-            console.log(err)
-        })
-    } 
+            .then(({ data }) => {
+                setSch(data.data)
+                setmeta_Sch(data.meta)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
-    
-    
-    useEffect(()=>{
+    const filters = async () => {
+        getSch()
+    }
+
+    useEffect(() => {
         getDetail()
-
-    },[])
-
-    useEffect(()=>{
         getSch()
-    },[])
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         getSch()
-    },[pickSch])
-    
+    }, [pickSch, pageactive])
 
     return (
         <div>
-            <Navbar/>
+            <Navbar />
 
             {detail ? (
-                detail.map((v)=>{
-                    return <MovieDetail title={v.title} genre={v.genres} release={v.release_date} directed={v.directors} hour={v.duration_hour} time={v.duration_minute} casts={v.casts} foto={v.image} bgFoto={v.cover_image} synopsis={v.synopsis}/>
+                detail.map((v) => {
+                    return <MovieDetail title={v.title} genre={v.genres} release={v.release_date} directed={v.directors} hour={v.duration_hour} time={v.duration_minute} casts={v.casts} foto={v.image} bgFoto={v.cover_image} synopsis={v.synopsis} />
                 })
-            ):(
+            ) : (
                 <h1>Data Not Found</h1>
             )}
 
             <div className="main-book">
-                <div className="hidden lg:flex text-2xl md:px-16 lg:px-20 w-full">
+                <div className="hidden lg:flex text-3xl md:px-16 lg:px-20 w-full font-semibold">
                     Book Tickets
                 </div>
 
                 <div className="flex lg:hidden text-center justify-center xs:-mt-10 xs:mb-5 md:mt-8 text-2xl md:px-16 lg:px-20 w-full">
-                    Showtimes and Tickets 
+                    Showtimes and Tickets
                 </div>
 
                 <div className="flex xl:justify-between xs:flex-col xs:gap-y-5 lg:flex-row lg:gap-x-5 w-full xs:px-10 md:px-16 lg:px-16 lg:mt-8">
-                    
-                <div className="choose-date flex flex-col">
-                    <label className="font-semibold lg:ms-4">Choose Date</label>
-                    <div className="border rounded-md bg-fontInput flex flex-row p-2 lg:ms-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
-                    </svg>
-                           
-
-                    <select 
-                    className="w-full lg:ms-5 p-0.5 text-black bg-fontInput outline-none"
-                    placeholder="Purwokerto">
-                    {   
-
-                        pickDate.map((v)=>(
-                        <option>{v}</option>
-                    ))}
-                    </select>
-                                    
-                    </div>
-                </div>
 
                     <div className="choose-date flex flex-col">
-                        <label className="font-semibold lg:ms-4">Choose Time</label>
+                        <label className="font-semibold lg:ms-4 mb-3">Choose Date</label>
+                        <div className="border rounded-md bg-fontInput flex flex-row p-2 lg:ms-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                            </svg>
+
+
+                            <select
+                                onChange={(e) => setdate(e.target.value)}
+                                className="w-full lg:ms-5 p-0.5 text-black bg-fontInput outline-none"
+                                placeholder="Purwokerto">
+                                {
+
+                                    pickDate.map((v) => (
+                                        <option>{v}</option>
+                                    ))}
+                            </select>
+
+                        </div>
+                    </div>
+
+                    <div className="choose-date flex flex-col">
+                        <label className="font-semibold lg:ms-4 mb-3">Choose Time</label>
                         <div className="border rounded-md bg-fontInput flex flex-row p-2 lg:ms-4">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                                 
 
-                        <select 
-                        className="w-full lg:ms-5 p-0.5 text-black bg-fontInput outline-none"
-                        placeholder="Purwokerto">
-                        {   
-    
-                            pickTime.map((v)=>(
-                            <option>{v}</option>
-                        ))}
-                        </select>
-                                         
+
+                            <select
+                                onChange={(e) => settime(e.target.value)}
+                                className="w-full lg:ms-5 p-0.5 text-black bg-fontInput outline-none"
+                                placeholder="Purwokerto">
+                                {
+                                    pickTime.map((v) => (
+                                        <option>{v}</option>
+                                    ))}
+                            </select>
+
                         </div>
-                </div>
+                    </div>
 
 
                     <div className="choose-date flex flex-col">
-                        <label className="font-semibold lg:ms-4">Choose Location</label>
+                        <label className="font-semibold lg:ms-4 mb-3">Choose Location</label>
                         <div className="border rounded-md bg-fontInput flex flex-row p-2 lg:ms-4">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                            </svg>           
+                            </svg>
 
-                            <select 
-                            className="w-full lg:ms-5 p-0.5 text-black bg-fontInput outline-none"
-                            placeholder="Purwokerto">
-                            {   
-        
-                                pickLoc.map((v)=>(
-                                <option>{v}</option>
-                            ))}
+                            <select
+                                onChange={(e) => setloc(e.target.value)}
+                                className="w-full lg:ms-5 p-0.5 text-black bg-fontInput outline-none"
+                                placeholder="Purwokerto">
+                                {
+                                    pickLoc.map((v) => (
+                                        <option>{capitalTitle(v)}</option>
+                                    ))}
                             </select>
-                                             
+
                         </div>
                     </div>
 
-                    <div className="filter rounded-md bg-button text-white xs:p-4 lg:p-0 lg:mt-6 lg:w-1/5 flex items-center justify-center ">
-                        <button>Filter</button>
-                    </div>
+                    <button className="filter rounded-md bg-button text-white xs:p-4 lg:p-0 lg:mt-9 ml-5 lg:w-1/5 flex items-center justify-center " onClick={filters}>Filter</button>
                 </div>
 
                 <div className="Choose-Cinema mt-5 lg:px-20">
                     <div className="text-cinema flex xs:flex-col lg:flex-row xs:w-full lg:w-1/4 justify-between">
                         <p className="font-semibold hidden lg:flex">Choose Cinema</p>
-                        <p className="text-font xs:text-center lg:text-normal">39 Result</p>
+                        <p className="text-font xs:text-center lg:text-normal">{meta_sch.total_data} Result</p>
                     </div>
 
                     <div className="flex xs:flex-col xs:gap-y-5 lg:gap-x-3 md:px-16 lg:gap-y-0 xs:px-10 lg:px-0 lg:flex-row mt-5 w-full justify-between">
-                        
-                    
-                        {sch ?
-                         sch.map((v)=>{
-                            return (
-                                <div>
-                                    <div className= { (pickSch == v.id_time_schedule ? 'border-button' : 'bg-white') + " border rounded-md cinema xs:p-3 xs:flex-col lg:p-16 lg:max-h-[120px] flex lg:justify-center lg:items-center"} onClick={()=>setPickSch(v.id_time_schedule)}>
-                                        <img src={v.image_premier} alt="" className="xs:max-h-[13px] lg:max-h-[150px] w-fit"/>
-                                        <p className="xs:flex lg:hidden mt-2 text-md font-semibold">{v.name_premier}</p>
-                                        <p className="xs:flex text-xs mt-2 text-font lg:hidden">{v.regency}</p>
-                                        <div className="my-3 flex w-full lg:hidden">
-                                            <hr className="w-full"/>
-                                        </div>
-                                        <div className="w-full flex flex-row justify-between lg:hidden">
-                                            <p className="text-lg font-semibold">Price</p>
-                                            <p>{v.price}</p>
-                                        </div>
 
-                                        <div className="w-full flex flex-row justify-between lg:hidden">
-                                            <p className="text-lg font-semibold">Date</p>
-                                            <p>{v.set_date}</p>
+
+                        {sch ?
+                            sch.map((v) => {
+                                return (
+                                    <div>
+                                        <div className={(pickSch == v.id_time_schedule ? 'border-button' : 'bg-white') + " hover:border-button border rounded-md cinema xs:p-3 xs:flex-col lg:p-16 lg:max-h-[120px] flex lg:justify-center lg:items-center"} onClick={() => setPickSch(v.id_time_schedule)}>
+                                            <img src={v.image_premier} alt="" className="xs:max-h-[25px] lg:max-h-[150px] w-fit" />
+                                            <p className="xs:flex lg:hidden mt-2 text-md font-semibold">{v.name_premier}</p>
+                                            <p className="xs:flex text-xs mt-2 text-font lg:hidden">{capitalTitle(v.regency)}</p>
+                                            <div className="my-3 flex w-full lg:hidden">
+                                                <hr className="w-full" />
+                                            </div>
+                                            <div className="w-full flex flex-row justify-between lg:hidden">
+                                                <p className="text-lg font-semibold">Price</p>
+                                                <p>{"Rp. " + v.price}</p>
+                                            </div>
+
+                                            <div className="w-full flex flex-row justify-between lg:hidden">
+                                                <p className="text-lg font-semibold">Date</p>
+                                                <p>{moment.utc(v.set_date).utc().format('MMMM D, YYYY')}</p>
+                                            </div>
+                                            <div className="w-full flex flex-row justify-between lg:hidden">
+                                                <p className="text-lg font-semibold">Time</p>
+                                                <p>{moment.utc(v.set_date).utc().format('MMMM D, YYYY')}</p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        }):(
-                            <div>Data Not Found</div>
-                        )}
+                                )
+                            }) : (
+                                <div>Data Not Found</div>
+                            )}
 
                     </div>
                 </div>
 
-                <div className="pagination mt-5 w-full mx-auto flex flex-row gap-x-5 justify-center items-center">
-                    <div className="px-3 py-2 border bg-button text-white rounded-md">
-                        1
-                    </div>
-
-                    <div className="px-3 py-2 border bg-white text-button rounded-md">
-                        2
-                    </div>
-
-                    <div className="px-3 py-2 border bg-white text-button rounded-md">
-                        3
-                    </div>
-
-                    <div className="px-3 py-2 border bg-white text-button rounded-md">
-                        4
-                    </div>
+                <div className="flex pt-10 justify-around">
+                    <Pagination meta={meta_sch} page_active={pageactive} set_page_active={setpageactive} />
                 </div>
 
-                <button className="w-full flex justify-center items-center mt-10" onClick={()=> navigate(`/order/${pickSch}`)}>
-                    <div className="text-white bg-button px-5 py-3 rounded-md">    
+                <button className="w-full flex justify-center items-center my-10" onClick={() => navigate(`/order/${pickSch}`)}>
+                    <div className="text-white bg-button px-5 py-3 rounded-md">
                         Book Now
                     </div>
                 </button>
 
             </div>
 
-            <Footer/>
+            <Footer />
         </div>
     )
 }
